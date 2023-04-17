5bf542e309feffe953680c831b3f3f5e87127e88
KaiRenken <kai.renken@gmx.de>
2023-04-17 08:33:24 +0200

    initial commit

---
+++ src/main/java/de/neusta/notebookjava/NotebookJavaApplication.java
@@ -0,0 +1,13 @@
+package de.neusta.notebookjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotebookJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotebookJavaApplication.class, args);
+    }
+
+}
