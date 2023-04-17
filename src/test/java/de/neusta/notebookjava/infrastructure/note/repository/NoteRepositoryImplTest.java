799f44707fbd8d933013851274016e40734ed287
KaiRenken <kai.renken@gmx.de>
2023-04-17 15:43:51 +0200

    fourth commit

--- src/test/java/de/neusta/notebookjava/infrastructure/note/repository/NoteRepositoryImplTest.java
+++ src/test/java/de/neusta/notebookjava/infrastructure/note/repository/NoteRepositoryImplTest.java
@@ -2,8 +2,11 @@ package de.neusta.notebookjava.infrastructure.note.repository;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.temporal.ChronoUnit;
+
 import static de.neusta.notebookjava.testdatafactories.NoteTestDataFactory.aTestNote;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.within;
 
 class NoteRepositoryImplTest {
 
@@ -21,10 +24,10 @@ class NoteRepositoryImplTest {
 
         assertThat(result).hasSize(2);
         assertThat(result.get(0).getId()).isEqualTo(note1ToStore.getId());
-        assertThat(result.get(0).getCreationDate()).isEqualTo(note1ToStore.getCreationDate());
+        assertThat(result.get(0).getCreationDate()).isCloseTo(note1ToStore.getCreationDate(), within(1, ChronoUnit.SECONDS));
         assertThat(result.get(0).getContent()).isEqualTo(note1ToStore.getContent());
         assertThat(result.get(1).getId()).isEqualTo(note2ToStore.getId());
-        assertThat(result.get(1).getCreationDate()).isEqualTo(note2ToStore.getCreationDate());
+        assertThat(result.get(1).getCreationDate()).isCloseTo(note2ToStore.getCreationDate(), within(1, ChronoUnit.SECONDS));
         assertThat(result.get(1).getContent()).isEqualTo(note2ToStore.getContent());
     }
 }
